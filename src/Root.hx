8212ebfeb38eaf53725278af3cb6add60cae6330
Salvatore Bottiglieri <sb797@nau.edu>
2015-01-26 14:24:47 -0700

    Added Paddle to environment space with correct position on circle

--- src/Root.hx
+++ src/Root.hx
@@ -14,6 +14,7 @@ class Root extends Sprite {
     public static var assets:AssetManager;
     public var ninja:Image;
     public var Circle_placeholder:Image;
+    public var Paddle:Image;
 
     public function new() {
         super();
@@ -24,6 +25,7 @@ class Root extends Sprite {
         assets = new AssetManager();
         assets.enqueue("assets/ninja.png");
         assets.enqueue("assets/Circle_placeholder.png");
+        assets.enqueue("assets/Paddle.png");
         assets.loadQueue(function onProgress(ratio:Float) {
 
             if (ratio == 1) {
@@ -34,58 +36,65 @@ class Root extends Sprite {
                         alpha: 0,
                         onComplete: function() {
                         startup.removeChild(startup.loadingBitmap);
-                        ninja = new Image(Root.assets.getTexture("ninja"));
-                        ninja.x = 100;
-                        ninja.y = 0;
-                        addChild(ninja);
+                        // ninja = new Image(Root.assets.getTexture("ninja"));
+                        // ninja.x = 100;
+                        // ninja.y = 0;
+                        // addChild(ninja);
 
                         Circle_placeholder = new Image(Root.assets.getTexture("Circle_placeholder"));
                         Circle_placeholder.x = 250;
                         Circle_placeholder.y = 250;
                         addChild(Circle_placeholder);
 
+                        Paddle = new Image(Root.assets.getTexture("Paddle"));
+                        Paddle.x = 200;
+                        Paddle.y = 200;
+                        addChild(Paddle);
+
                         Starling.current.stage.addEventListener(KeyboardEvent.KEY_DOWN,
                             function(event:KeyboardEvent) {
                                 if (event.keyCode == Keyboard.LEFT) {
-                                    var position = ninja.x - 10;
-                                    Starling.juggler.tween(ninja, 0.06, {
+                                    var position = Paddle.x - 1;
+                                    Starling.juggler.tween(Paddle, 0.06, {
                                         transition: Transitions.LINEAR,
                                         x: position
                                         });
                                 }
                                 if (event.keyCode == Keyboard.RIGHT) {
-                                    var position = ninja.x + 10;
-                                    Starling.juggler.tween(ninja, 0.06, {
+                                    var position = Paddle.x + 1;
+                                    Starling.juggler.tween(Paddle, 0.06, {
                                         transition: Transitions.LINEAR,
                                         x: position
                                         });                                }
                                 if (event.keyCode == Keyboard.DOWN) {
-                                    var position = ninja.y + 10;
-                                    Starling.juggler.tween(ninja, 0.06, {
+                                    var position = Paddle.y + 1;
+                                    Starling.juggler.tween(Paddle, 0.06, {
                                         transition: Transitions.LINEAR,
                                         y: position
                                         });    
                                 }
                                 if (event.keyCode == Keyboard.UP) {
-                                    var position = ninja.y - 10;
-                                    Starling.juggler.tween(ninja, 0.06, {
+                                    var position = Paddle.y - 1;
+                                    Starling.juggler.tween(Paddle, 0.06, {
                                         transition: Transitions.LINEAR,
                                         y: position
                                         });  
                                 }
+                                // trace(Paddle.x);
+                                // trace(Paddle.y);
                                 });
 
-                        ninja.addEventListener(TouchEvent.TOUCH,
-                            function(e:TouchEvent) {
-                                var touch = e.getTouch(stage, TouchPhase.BEGAN);
-                                if (touch == null) return;
-                                });
+                        // ninja.addEventListener(TouchEvent.TOUCH,
+                        //     function(e:TouchEvent) {
+                        //         var touch = e.getTouch(stage, TouchPhase.BEGAN);
+                        //         if (touch == null) return;
+                        //         });
 
-                        Starling.juggler.tween(ninja, 1.0, {
-                            transition: Transitions.EASE_OUT_BOUNCE,
-                                delay: 2.0,
-                                y: 250
-                                });
+                        // Starling.juggler.tween(ninja, 1.0, {
+                        //     transition: Transitions.EASE_OUT_BOUNCE,
+                        //         delay: 2.0,
+                        //         y: 250
+                        //         });
 
                     }
 
